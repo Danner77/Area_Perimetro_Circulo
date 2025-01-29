fd1497eda77357e8422cbedf98fa68cac2f369ef
Danner77 <danielvillar@colegioguanenta.edu.co>
2025-01-29 07:19:14 -0500

    se agrego codigo python

---
+++ area_perimetro_circulo.py
@@ -0,0 +1,20 @@
+import math
+
+print("............................")
+print("area perimetro de el circulo")
+print("............................")
+
+
+# input
+r=input ("digite el valor de el radio:  ")
+r = int(r)
+
+# procesing
+p=2*math.pi*r
+a= math.pi*r**2
+
+# output
+print("..........................")
+print("el area es : " + str(a))
+print("el perimetro es: " + str(p))
+print("..........................")
